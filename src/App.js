e63443e0189049fb12670f2ec37e28e63ed982a3
priyankabelkhede <priyanka291104b@gmail.com>
2022-09-03 17:17:41 +0530

    made change in anchor tag

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <h1>hello world</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/priyankabelkhede"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit my github
         </a>
       </header>
     </div>
